5a82336a846f81f4657f5f79ad383d191ff9b1fd
Juan Ibarra <jmibarra86@gmail.com>
2023-03-27 19:15:15 -0300

    Luego de investigar y hacer pruebas mejor el tokenizer a toktok para que tokenice los signos de apertura

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,13 +1,17 @@
 import nltk
 from nltk.stem import SnowballStemmer
+from nltk.tokenize.toktok import ToktokTokenizer
+
 # Solo la primera vez / Paquete con un tokenizer para entrenar
 nltk.download('punkt')
+
+toktok = ToktokTokenizer()
 
 stemmer = SnowballStemmer('spanish')
 
 
 def tokenize(sentense):
-    return nltk.word_tokenize(sentense)
+    return toktok.tokenize(sentense)
 
 
 def stem(word):
@@ -16,14 +20,3 @@ def stem(word):
 
 def bag_of_words(tokenized_sentence, all_words):
     pass
-
-
-a = "Hola, ¿en qué puedo ayudarte?"
-print(a)
-a = tokenize(a)
-print(a)
-
-# Tengo que investigar por que al parecer no funciona con palabras en español
-words = ['organizar', 'organización', 'organizado']
-stemmed_words = [stem(word)for word in words]
-print(stemmed_words)

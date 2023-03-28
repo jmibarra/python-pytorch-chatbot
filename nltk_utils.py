256eed1850b5dfe7ee576a5378b161274e12046e
Juan Ibarra <jmibarra86@gmail.com>
2023-03-28 19:10:48 -0300

    Se arma el modelo que vamos a usar para entrenar el bot y probamos el entrenamiento

--- nltk_utils.py
+++ nltk_utils.py
@@ -21,7 +21,6 @@ def stem(word):
 
 def bag_of_words(tokenized_sentence, all_words):
     tokenized_sentence = [stem(w) for w in tokenized_sentence]
-    print(tokenized_sentence)
 
     bag = np.zeros(len(all_words), dtype=np.float32)
     for idx, w, in enumerate(all_words):
--- train.py
+++ train.py
@@ -6,6 +6,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from model import NeuralNet
+
 with open('intents.json', 'r') as file:
     intents = json.load(file)
 
@@ -45,23 +47,58 @@ y_train = np.array(y_train)
 
 
 class ChatDataset(Dataset):
+
     def __init__(self):
         self.n_samples = len(X_train)
         self.x_data = X_train
         self.y_data = y_train
 
-        # dataset[idx]
-        def __getItem__(self, index):
-            return self.x_data[index], self.y_data[index]
+    # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
 
-        def __len__(self):
-            return self.n_samples
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.n_samples
 
 
 # Hyperparametros
 batch_size = 8
-
+hidden_size = 8
+output_size = len(tags)
+input_size = len(X_train[0])  # Todas tienen el mismo tamaño
+learning_rate = 0.001
+num_epocs = 1000
 
 dataset = ChatDataset()
-train_loader = DataLoader(
-    dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available()
+                      else 'cpu')  # Si hay GPU lo utilizo sino CPU
+model = NeuralNet(input_size, hidden_size, output_size)
+
+# loss and optimización
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epocs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+
+        # FW
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        # backwards and optimización
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch + 1) % 100 == 0:
+        print(f'epoch {epoch+1}/{num_epocs}, loss={loss.item():.4f}')
+
+print(f'Final loss, loss={loss.item():.4f}')
